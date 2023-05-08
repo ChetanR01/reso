fc758d162f527cb52b77231f970bd206aaec40bd
ChetanR01 <chetan0101career@gmail.com>
2023-05-09 00:17:06 +0530

    initial commit

---
+++ resoapp/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Menu, Contact, BookTable
+# Register your models here.
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ['name','price','category']
+    list_editable = ['price']
+    list_filter = ('category',)
+admin.site.register(Menu, MenuAdmin)
+
+class BookTableAdmin(admin.ModelAdmin):
+    list_display = ['name','date','sits']
+    list_filter = ('date',)
+admin.site.register(BookTable, BookTableAdmin)
+
+admin.site.register(Contact)
